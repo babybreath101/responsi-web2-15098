7c8447fed396b24a6105c68d12136df13ccc2fcf
babybreath101 <annastasyafar04@gmail.com>
2019-12-17 14:32:05 +0700

    first commit

---
+++ app/Jobs.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Jobs extends Model
+{
+    protected $table = 'jobs';
+
+    protected $primaryKey = "id_jobs";
+    protected $fillable= ['name',];
+
+    public $timestamps = true;
+
+    
+}
---
+++ resources/views/annastasya.blade.php
@@ -0,0 +1,8 @@
+@extends('base')
+@section('employees','active')
+@section('content')
+
+<h1> Nama Lengkap = Annastasya Fitri Amalia R </h1>
+<h1> NIM = 18/425956/SV/15098 </h1>
+
+@endsection
